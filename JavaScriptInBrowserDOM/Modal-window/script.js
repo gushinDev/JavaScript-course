82233ebeb0a94823aa8afe6677bd7889e8be03ed
gushinDev <gushindev@gmail.com>
2022-01-14 13:42:28 +0300

    modal window

--- JavaScriptInBrowserDOM/Modal-window/script.js
+++ JavaScriptInBrowserDOM/Modal-window/script.js
@@ -1,1 +1,43 @@
 'use strict';
+
+const buttons = document.querySelectorAll('button');
+const modalWindow = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+buttons.forEach(button => {
+
+  if (button.classList.contains('show-modal')) {
+    button.addEventListener('click', (evt) => {
+      console.log(evt);
+      removeClassHidden(modalWindow, overlay);
+    });
+  }
+  if (button.classList.contains('close-modal')) {
+    button.addEventListener('click', () => {
+      addClassHidden(modalWindow, overlay);
+    });
+  }
+
+});
+
+overlay.addEventListener('click', () => {
+  addClassHidden(modalWindow, overlay);
+})
+
+document.addEventListener('keydown', (key) => {
+  if(key.key === 'Escape'){
+    addClassHidden(modalWindow, overlay);
+  }
+})
+
+function addClassHidden(...args) {
+  args.forEach(element => {
+    element.classList.add('hidden');
+  });
+}
+
+function removeClassHidden(...args) {
+  args.forEach(element => {
+    element.classList.remove('hidden');
+  });
+}
